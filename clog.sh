6e45d99e7316cbcdfbb2738bb7831f6ecca33eee
mickaelreil <michael.reilhac@live.fr>
2019-12-04 15:36:15 +0100

    add unique

--- clog.sh
+++ clog.sh
@@ -2,20 +2,35 @@
 # Prétraitement des fichiers logs au format IDD_LOG_APP
 #Déclarations des variables
 input=$1
+src=$(cat $1 | sort -u | sed "/^[[:space:]]*$/d")
 output=$2
 month=$(ls -la --time-style=+%Y-%m-%d $1 | awk '{print $6}' | cut -d "-" -f 2)
 year=$(ls -la --time-style=+%Y-%m-%d $1 | awk '{print $6}' | cut -d "-" -f 1)
 dest="$output/$month-$year"
 pwd=${0%/*}
+dest_filename="log-$(date +%m-%d-%y).txt"
+output=""
+
+if [ -f "$dest/$dest_filename" ]; then
+	currentfile=$(echo "$src" | sha256sum | sed "s/[^a-z0-9]//g")
+	if [[ ! -z $(grep $currentfile '.lastfilesha256') ]]; then
+		echo "Le fichier a déjà été prétraité."
+		exit 1
+	else
+		output=$(cat "$dest/$dest_filename")
+	fi
+fi
+
 if [ $pwd = $0 ]; then
 	pwd="."
 fi
 if [ $1 = '' ] && [ $2 = '' ]; then
 	echo "Entrez un fichier source et un répertoire de destination."
+	exit 1
 fi
 
 if [ ! -d "$dest" ]; then
-	mkdir -p "$dest"
+	mkdir "$dest"
 fi
 
 if [ ! -f "$input" ]; then
@@ -57,10 +72,18 @@ then
 	echo "Erreur de format."
 	exit 1
 fi
+
 # Lecture/Vérification du fichier d'entrée
 # 'sort -u' supprime les doublons, voire plus
-cat $input | sort -u | while IFS= read -r line
+while IFS= read -r line
 do
+	newline=$(bash "$pwd/check_format.sh" "$line" "$regexp" "$config")
+	output=$output$newline$'\n'
+done <<< "$src"
 
-	bash "$pwd/check_format.sh" "$line" "$regexp" "$config"  >> "$dest/log-$(date +%m-%d-%y).txt"
-done
+#Création du fichier de prétraitement
+echo "$output" | sort -u | sed "/^[[:space:]]*$/d" > "$dest/$dest_filename"
+
+#Signature du fichier log
+echo "$src" | sha256sum | sed "s/[^a-z0-9]//g" > ".lastfilesha256"
+
